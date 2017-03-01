68d9af0313966d47bd18e1a2c81fb640079e1dcf
inner89 <xu8916235012>
2017-03-01 20:51:36 +0800

    I init

---
+++ src/main/java/com/xuyg/tinyframework/MyServlet.java
@@ -0,0 +1,7 @@
+package com.xuyg.tinyframework;
+
+/**
+ * Created by XYG on 2017-03-01.
+ */
+public class MyServlet {
+}
---
+++ src/main/java/com/xuyg/tinyframework/util/CalssUtil.java
@@ -0,0 +1,7 @@
+package com.xuyg.tinyframework.util;
+
+/**
+ * Created by XYG on 2017-03-01.
+ */
+public class CalssUtil {
+}
